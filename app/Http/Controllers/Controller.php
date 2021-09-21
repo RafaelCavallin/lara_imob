1f21469649115636daf224477e89081ff8f6e9cd
rafael cavallin <rafael_cavallin@hotmail.com>
2021-09-21 15:28:47 -0300

    First commit on this project

---
+++ app/Http/Controllers/Controller.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace LaraDev\Http\Controllers;
+
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+use LaraDev\Support\Message;
+
+class Controller extends BaseController
+{
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    protected $message;
+
+    public function __construct()
+    {
+        $this->message = new Message();
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+ */
+
+/* Route::get('/', function () {
+return view('welcome');
+}); */
+
+Route::group(['prefix' => 'admin', 'namespace' => 'Admin', 'as' => 'admin.'], function () {
+    /** Formulário de Login */
+    Route::post('/login', 'AuthController@login')->name('login.do');
+    Route::get('/', 'AuthController@showLoginForm')->name('login');
+
+    /** Rotas protegidas */
+    Route::group(['middleware' => ['auth']], function () {
+        Route::get('home', 'AuthController@home')->name('home');
+        Route::get('users/team', 'UserController@team')->name('users.team');
+        Route::resource('users', 'UserController');
+    });
+
+    /** Logout */
+    Route::get('logout', 'AuthController@logout')->name('logout');
+
+});
